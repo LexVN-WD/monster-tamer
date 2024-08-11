fd6444ceb963d513f6ccdbdc25df6bca89e22ed5
Lex Van-Nugent <lexvn.wd@gmail.com>
2024-08-11 13:21:54 -0600

    initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Main starting point of game, everything needed to start the game is here
+
+import Phaser from './lib/phaser.js';
+
+const game = new Phaser.Game({
+  parent: 'game-container',
+
+});
